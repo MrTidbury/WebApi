de20b4fb323be87f97b3da329b807d9ab6224670
jack <jack@tidbury.xyz>
2016-11-28 14:58:43 +0000

    Added New A better layout to Filesystem

--- modules/getdata.js
+++ modules/getdata.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const request = require('request')
+const apiKey = require('../secrets').apiKey
 
 exports.recipeSearch = function recipeSearch(req, res, next) {
 	console.log("fucntion called")
   const q = req.query.q
-  const apiKey = "IjxXa3lYuOmshmPo6MNAOhXRj1uSp1EuRcsjsnpfE2jyi3RS2c"
-	
+
 	var options = {
   url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=100&offset=0&query='+q,
   headers: {
@@ -17,11 +17,12 @@ exports.recipeSearch = function recipeSearch(req, res, next) {
     if (!error && response.statusCode === 200) {
 			const recipes = []
 			const results = JSON.parse(body).results
-			for (let i = 0; i < results.length; i++) {
+			for (let i = 0; i < 20; i++) {
 				const recipe = {
 					id: results[i].id,
 					title: results[i].title,
-					readyInMinutes: results[i].readyInMinutes
+					readyInMinutes: results[i].readyInMinutes,
+					FindMore: 'http://localhost:8080/recipe/'+results[i].id
 				}
 
 				recipes.push(recipe)
@@ -31,9 +32,5 @@ exports.recipeSearch = function recipeSearch(req, res, next) {
 			res.send(501, {message: 'Problem with SpoonAcular API query.', error: error, statusCode: response.statusCode})
 		}
 	})
-         
-}
 
-exports.test = function test(req, res, next) {
-res.send(200,"This is a test")    
 }
