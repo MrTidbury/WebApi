63d8077adcc0a3acb83baa96193b7c648001272b
jack <jack@tidbury.xyz>
2016-11-30 10:51:24 +0000

    Added Add user Function to the Database, all tests pass

--- modules/database.js
+++ modules/database.js
@@ -6,5 +6,26 @@ const MongoClient = require('mongodb').MongoClient,
 
 // Use connect method to connect to the Server
 exports.adduser = function adduser(name, email, passwordHash, favorites){
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err)
+		console.log('Connected correctly to server')
+		insertDocument(db, function() {
+			db.close()
+		})
+	})
 
+	const insertDocument = function(db, callback) {
+		db.collection('users').insertOne( {
+			'name': name,
+			'email': email,
+			'favorites': favorites,
+			'passowordHash': passwordHash,
+		}, function(err, result) {
+			assert.equal(err, null)
+			console.log('Inserted a document into the restaurants collection.')
+			callback(result.ops)
+			console.log(result.ops)
+			return result.ops
+		})
+	}
 }
