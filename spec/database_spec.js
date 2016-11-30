f40069a13afb5ac75061ee3daf0bb48a878ec42f
jack <jack@tidbury.xyz>
2016-11-30 18:51:02 +0000

    Updated tests to use the new mongoose module

--- spec/database_spec.js
+++ spec/database_spec.js
@@ -5,7 +5,7 @@ describe('Add user to db', function() {
 	it('returns added user', function(done) {
 		const returnData = database.adduser('Adam','Mesdsd','adsdafe23232321',[12122,2,2,3])
 
-		expect(returnData).toBeUndefined()
+		expect(returnData.name).toBe('Adam')
 		done()
 
 	})
