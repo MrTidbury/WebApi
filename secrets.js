a5c3bb4ef4bcf5116e53113a1424bcbfdd498e28
Jack Tidbury <mrtidbury@gmail.com>
2016-12-12 14:07:32 +0000

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-exports.apiKey="40B6hsXWgpmshOkpqQoDwKzzZRHSp1GQgyOjsn8BRx833wBeeO"
-exports.dbUrl="mongodb://root:root@ds113938.mlab.com:13938/users"
-exports.emailaddr = "api@tidbury.xyz"
-exports.emailpsw  = "apiemail1234"
